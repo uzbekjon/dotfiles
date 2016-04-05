a74ead350f6d819cb8ea3c96253720dcc39b78df
Uzbekjon <uzbekjon@gmail.com>
2016-04-05 07:58:01 +0500

    Install Homebrew if not installed.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,13 @@
-# Check if on OSX
-if [[ $OSTYPE == darwin* ]]; then
-    
-    ln -fs "${HOME}/.dotfiles/bash_profile" "${HOME}/.bash_profile"
+#!/bin/bash
 
-fi
+DOTFILES_DIR="${HOME}/.dotfiles"
+
+# Create .bash_profile symlink in home directory
+ln -fs "${DOTFILES_DIR}/bash_profile" "${HOME}/.bash_profile"
+
+# Check if on OSX
+case $OSTYPE in
+    darwin*)
+        source "${DOTFILES_DIR}/bin/install-osx.sh"
+    ;;
+esac
---
+++ bin/install-osx.sh
@@ -0,0 +1,14 @@
+# Taken from here:
+#   https://gist.github.com/phatblat/1713458
+
+#
+# Check if Homebrew is installed
+#
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
