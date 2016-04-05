2b4dd535925d7187f4eb48977e4a9c6e016379fd
Uzbekjon <uzbekjon@gmail.com>
2016-04-05 23:38:52 +0500

    Add .gemrc file on bootstrap.

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -5,6 +5,7 @@ DOTFILES_DIR="${HOME}/.dotfiles"
 # Create .bash_profile and .gitconfig symlinks in home directory
 ln -ivs "${DOTFILES_DIR}/bash_profile" "${HOME}/.bash_profile"
 ln -ivs "${DOTFILES_DIR}/gitconfig"    "${HOME}/.gitconfig"
+ln -ivs "${DOTFILES_DIR}/gemrc"        "${HOME}/.gemrc"
 
 # Check if on OSX
 case $OSTYPE in
